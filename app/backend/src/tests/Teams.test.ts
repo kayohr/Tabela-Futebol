ff1dc602ab27b33a5e6ae7203ac79fc617eecde0
kayo <kayohramos@gmail.com>
2023-04-04 18:38:57 -0300

    tests

---
+++ app/backend/src/tests/Teams.test.ts
@@ -0,0 +1,112 @@
+import * as chai from 'chai';
+import * as sinon from 'sinon';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+import Teams from "../database/models/Teams";
+import { teamsMock } from "./mock/team.mock";
+
+chai.use(chaiHttp)
+
+import { app } from '../app';
+
+import { Response } from 'superagent';
+import Users from '../database/models/Users';
+import { users } from './mock/users.mock';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+// describe('', () => {
+//     describe('', () => {
+//         it('', async () => {
+//             const httpResponse = await chai.request(app).post('/login'.send({}))
+//             expect(httpResponse.status).to.equal(400)
+//         })
+//     })
+// })
+describe('', () => {
+describe('teams', () => {
+   
+    let chaiHttpResponse : Response ;
+  
+    before(async () => {
+      sinon
+        .stub(Teams, "findAll")
+        .resolves(
+            teamsMock as Teams[]
+         );
+    });
+  
+    after(()=>{
+      (Teams.findAll as sinon.SinonStub).restore();
+    })
+  
+    it('...', async () => {
+      chaiHttpResponse = await chai
+         .request(app)
+         .get('/teams')
+  
+      expect(chaiHttpResponse.status).to.be.equal(200)
+      expect(chaiHttpResponse.body).to.deep.equal(teamsMock)
+
+    });
+  
+    describe('Seu teste', () => {
+   
+        let chaiHttpResponse : Response ;
+      
+        before(async () => {
+          sinon
+            .stub(Teams, "findOne")
+            .resolves(
+                teamsMock as any
+             );
+        });
+      
+        after(()=>{
+          (Teams.findAll as sinon.SinonStub).restore();
+        })
+      
+        it('...', async () => {
+          chaiHttpResponse = await chai
+             .request(app)
+             .get('/teams')
+      
+          expect(chaiHttpResponse.status).to.be.equal(200)
+          expect(chaiHttpResponse.body).to.deep.equal(teamsMock[0])
+    
+        });
+    })
+  
+  });
+  describe(' users', () => {
+   
+    let chaiHttpResponse : Response ;
+  
+    before(async () => {
+      sinon
+        .stub(Users, "findOne")
+        .resolves(
+          users as any
+         );
+    });
+  
+    after(()=>{
+      (Users.findAll as sinon.SinonStub).restore();
+    })
+  
+    it('...', async () => {
+      chaiHttpResponse = await chai
+         .request(app)
+         .get('/login')
+  
+      expect(chaiHttpResponse.status).to.be.equal(200)
+      expect(chaiHttpResponse.body).to.deep.equal(users)
+
+    });
+})
+
+});
+
+  

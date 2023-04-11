55ac686b7a5e1711cd0c2c7659383ae028932a04
kayo <kayohramos@gmail.com>
2023-04-11 13:17:12 -0300

    req23, 24 e 25

--- app/backend/src/controllers/leaderboard.home.controller.ts
+++ app/backend/src/controllers/leaderboard.home.controller.ts
@@ -2,9 +2,13 @@ import { Request, Response } from 'express';
 import lederboardHome from '../services/leaderboard.home.services';
 
 const getLederboardHome = async (_req: Request, res: Response) => {
-  const Lederboard = await lederboardHome.getAllLeaderboardHome();
+  try {
+    const Lederboard = await lederboardHome.getAllLeaderboardHome();
 
-  return res.status(200).json(Lederboard);
+    return res.status(200).json(Lederboard);
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 export default {
--- app/backend/src/query/query.ts
+++ app/backend/src/query/query.ts
@@ -1,60 +1,33 @@
-const leaderBoardHome = `SELECT
-t.team_name AS name,
-SUM(p.points) AS totalPoints,
-COUNT(*) AS totalGames,
+const leaderBoardHome = `SELECT 
+teams.team_name AS name,
 SUM(CASE
-  WHEN t.id = m.home_team_id 
-  AND m.home_team_goals > m.away_team_goals 
-  OR t.id = m.away_team_id 
-  AND m.away_team_goals > m.home_team_goals THEN 1
-  ELSE 0
-END) AS totalVictories,
-SUM(CASE
-  WHEN m.home_team_goals = m.away_team_goals THEN 1
-  ELSE 0
-END) AS totalDraws,
-SUM(CASE
-  WHEN t.id = m.home_team_id 
-  AND m.home_team_goals < m.away_team_goals 
-  OR t.id = m.away_team_id 
-  AND m.away_team_goals < m.home_team_goals THEN 1
-  ELSE 0
-END) AS totalLosses,
-SUM(CASE
-  WHEN t.id = m.home_team_id THEN m.home_team_goals
-  ELSE m.away_team_goals
-END) AS goalsFavor,
-SUM(CASE
-  WHEN t.id = m.home_team_id THEN m.away_team_goals
-  ELSE m.home_team_goals
-END) AS goalsOwn,
-SUM(CASE
-  WHEN t.id = m.home_team_id THEN m.home_team_goals
-  ELSE m.away_team_goals
-END) - SUM(CASE
-  WHEN t.id = m.home_team_id THEN m.away_team_goals
-  ELSE m.home_team_goals
-END) AS goalsBalance,
-ROUND(SUM(p.points) / (COUNT(*) * 3) * 100, 2) AS efficiency
-FROM TRYBE_FUTEBOL_CLUBE.teams t
-INNER JOIN TRYBE_FUTEBOL_CLUBE.matches m 
-ON t.id 
-IN (m.home_team_id, m.away_team_id)
-INNER JOIN (
-SELECT
-  m.id,
-  CASE
-    WHEN m.home_team_goals > m.away_team_goals THEN 3
-    WHEN m.home_team_goals < m.away_team_goals THEN 0
+    WHEN matches.home_team_goals > matches.away_team_goals THEN 3
+    WHEN matches.home_team_goals < matches.away_team_goals THEN 0
     ELSE 1
-  END AS points
-FROM TRYBE_FUTEBOL_CLUBE.matches m
-WHERE m.in_progress = FALSE
-) p ON m.id = p.id
+END) AS totalPoints,
+COUNT(*) AS totalGames,
+SUM(matches.home_team_goals > matches.away_team_goals) AS totalVictories,
+SUM(matches.home_team_goals = matches.away_team_goals) AS totalDraws,
+SUM(matches.home_team_goals < matches.away_team_goals) AS totalLosses,
+SUM(matches.home_team_goals) AS goalsFavor,
+SUM(matches.away_team_goals) AS goalsOwn,
+SUM(matches.home_team_goals) - SUM(matches.away_team_goals) AS goalsBalance,
+ROUND(SUM(CASE
+            WHEN matches.home_team_goals > matches.away_team_goals THEN 3
+            WHEN matches.home_team_goals < matches.away_team_goals THEN 0
+            ELSE 1
+        END) / (COUNT(*) * 3) * 100,
+        2) AS efficiency
+FROM
+TRYBE_FUTEBOL_CLUBE.teams AS teams
+    INNER JOIN
+TRYBE_FUTEBOL_CLUBE.matches AS matches ON teams.id = matches.home_team_id
+WHERE
+matches.in_progress = FALSE
 GROUP BY name
-ORDER BY totalPoints DESC, 
-totalVictories DESC, 
-goalsBalance DESC, 
-goalsFavor DESC;`;
+ORDER BY totalPoints DESC , 
+totalVictories DESC , 
+goalsBalance DESC , 
+goalsFavor DESC`;
 
 export default leaderBoardHome;

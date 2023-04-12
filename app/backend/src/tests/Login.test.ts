b6c932572a986d94e7a3ff62a9ae611bc46e383a
kayo <kayohramos@gmail.com>
2023-04-11 21:39:04 -0300

    testes até a 11

--- app/backend/src/tests/Login.test.ts
+++ app/backend/src/tests/Login.test.ts
@@ -1,113 +1,44 @@
-// import * as chai from 'chai';
-// import * as sinon from 'sinon';
-// // @ts-ignore
-// import chaiHttp = require('chai-http');
-// import Teams from "../database/models/Teams";
-// import { teamsMock } from "./mock/team.mock";
 
-// chai.use(chaiHttp)
+ 
 
-// import { app } from '../app';
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
 
-// import { Response } from 'superagent';
-// import Users from '../database/models/Users';
-// import { users } from './mock/users.mock';
-// import Matches from '../database/models/Matches';
-// import matchers from '@testing-library/jest-dom/matchers';
-// import { validLogin, validToken } from '../middlewares/validLogin';
+import { app } from '../app';
+import Users from '../database/models/Users';
+import { Response } from 'superagent';
+import { users } from '../tests/mock/users.mock'
+import { validToken } from '../middlewares/validLogin';
 
-// chai.use(chaiHttp);
+chai.use(chaiHttp);
 
-// const { expect } = chai;
+const { expect } = chai;
 
-// describe.only('', () => {
-// describe('login', () => {
+
+  describe.only('', () => {
+    let chaiHttpResponse: Response;
+
+    beforeEach(async () => {
+      sinon.stub(Users, 'findOne')
+        .resolves(users[1] as Users);
+    });
+
+    afterEach(() => {
+      (Users.findOne as sinon.SinonStub).restore();
+    })
+
+  
+    it('post login  ', async () => {
+      chaiHttpResponse = await chai.request(app)
+      .post('/login')
+      .send({
+        'email': 'admin@admin.co',
+        'password': 'secret_admin'
+      });
+      expect(chaiHttpResponse.status).to.be.equal(401);
+      expect(chaiHttpResponse.body).to.deep.equal({ "message": "Invalid email or password" })
+    })
    
-//     let chaiHttpResponse : Response ;
-  
-//     beforeEach(async () => {
-//       sinon
-//         .stub(Users, "findAll")
-//         .resolves(
-//             users as Users[]
-//          );
-//     });
-  
-//     afterEach(()=>{
-//       (Users.findAll as sinon.SinonStub).restore();
-//     })
-  
-//     it('Post Login ', async () => {
-//       chaiHttpResponse = await chai
-//          .request(app)
-//          .post('/login')
-         
-  
-//       expect(chaiHttpResponse.status).to.be.equal(200)
-//       expect(chaiHttpResponse.body).to.deep.equal('token')
-
-//     });
-  
-//     describe('Seu teste', () => {
-   
-//         let chaiHttpResponse : Response ;
-      
-//         beforeEach(async () => {
-//           sinon
-//             .stub(Users, "findOne")
-//             .resolves(
-//                 teamsMock as any
-//              );
-//         });
-      
-//         afterEach(()=>{
-//           (Users.findAll as sinon.SinonStub).restore();
-//         })
-        
-//         // it('...', async () => {
-//         //     chaiHttpResponse = await chai
-//         //     .request(app)
-//         //     .post('/login')
-//         //     .send(validToken)
-//         //     expect(chaiHttpResponse.status).to.be.equal(200)
-//         //     expect(chaiHttpResponse.body).to.have.property('token');
-//         //   });
-          
-//         //   it('...', async () => {
-//         //     chaiHttpResponse = await chai
-//         //        .request(app)
-//         //        .get('/')
-//         //        .send(validLogin)
-  
-//         //     expect(chaiHttpResponse.status).to.be.equal(200)
-//         //     expect(chaiHttpResponse.body).to.deep.equal('email e password')
-      
-//         //   });
-//         it('...', async () => {
-//           chaiHttpResponse = await chai
-//              .request(app)
-//              .get('/login/role')
-//              .set('Authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7ImlkIjoyLCJ1c2VybmFtZSI6IlVzZXIiLCJyb2xlIjoidXNlciIsImVtYWlsIjoidXNlckB1c2VyLmNvbSIsInBhc3N3b3JkIjoiJDJhJDA4JFk4QWJpOGpYdnNYeXFtLnJtcDBCLnVRQkE1cVV6N1Q2R2hsZy9DdlZyL2dMeFlqNVVBWlZPIn0sImlhdCI6MTY4MDYzOTc1OSwiZXhwIjoxNjgzMjMxNzU5fQ.5fC5evs0JphbXtjHNA8L4RZYKgA6xs7mk-AvYFFNX3c')
-      
-//           expect(chaiHttpResponse.status).to.be.equal(401)
-//           expect(chaiHttpResponse.body).to.deep.equal({ "message": "Token not found" }
-//           )
-    
-//         });
-      
-//         it('...', async () => {
-//           chaiHttpResponse = await chai
-//              .request(app)
-//              .get('/login/role')
-//              .set('Authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7ImlkIjoyLCJ1c2VybmFtZSI6IlVzZXIiLCJyb2xlIjoidXNlciIsImVtYWlsIjoidXNlckB1c2VyLmNvbSIsInBhc3N3b3JkIjoiJDJhJDA4JFk4QWJpOGpYdnNYeXFtLnJtcDBCLnVRQkE1cVV6N1Q2R2hsZy9DdlZyL2dMeFlqNVVBWlZPIn0sImlhdCI6MTY4MDYzOTc1OSwiZXhwIjoxNjgzMjMxNzU5fQ.5fC5evs0JphbXtjHNA8L4RZYKgA6xs7mk-AvYFFNX3c')
-      
-//           expect(chaiHttpResponse.status).to.be.equal(401)
-//           expect(chaiHttpResponse.body).to.deep.equal({ "message": "Token must be a valid token" })
-    
-//         });
-//     })
-  
-//   });
-
-
-// });
+  })
